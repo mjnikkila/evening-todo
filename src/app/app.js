7836f048384521801c1cc9bc851582bfb8aed4ec
Mika Nikkilä <git.commit.mn@td3.pw>
2016-10-31 22:57:02 +0200

    Changed way to handle layouts and implemented local storage support

--- src/app/app.js
+++ src/app/app.js
@@ -3,21 +3,39 @@ module.exports = function() {
     var v_layout = require("view/layout/layout");
     var v_tasklist = require("view/tasklist/tasklist");
 
-    var layout = new v_layout({
+    var task_collection = new c_task.collection();
+    Evening.repositoryAdd(task_collection, "tasks");
+
+    new v_layout({
         name: "layout",
         el: "body"
     });
 
-    // Generate some test data for app
-    var task_collection = new c_task.collection([
-        //{"title": "Task 1", "description": "This is task 1"},
-        //{"title": "Task 2", "description": "This is task 2"}
-    ]);
-
     // Render app main view
     new v_tasklist({
         name: "tasklist",
-        el: layout.$el.find(".main"),
+        el: Evening.repositoryGet("view", "layout").$el.find(".main"),
         collection: task_collection
+    });
+
+    /*
+     * Implement local storage capabilities
+     * @see https://github.com/tastejs/todomvc/blob/master/app-spec.md#persistence
+     *
+     * We differ a bit from the guidelines since I see that Lockr is better library to use
+     * with evening models since it can store objects too
+     */
+    var Lockr = require("lockr");
+
+    // Bootstrap tasks collection with saved data if any
+    task_collection.set(Lockr.get("todos-evening"));
+
+    // Set event handler for collection changes to automatically save tasks
+    task_collection.on("add remove change", function(e){
+        var models = [];
+        _.each(e.collection.models, function(model){
+            models.push(model.attributes);
+        });
+        Lockr.set("todos-evening", models);
     });
 };
